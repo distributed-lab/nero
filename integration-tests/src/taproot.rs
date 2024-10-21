c6c64694dbe81630c1107d7a9efb9828d25a0128
Velnbur <kyrylo.baybula@distributedlab.com>
2024-10-21 11:39:29 +0300

    Small refactor of tests

--- integration-tests/src/taproot.rs
+++ integration-tests/src/taproot.rs
@@ -1,12 +1,12 @@
-use std::{fs, str::FromStr as _};
+use std::{env, fs, str::FromStr as _};
 
 use bitcoin::{
     consensus::{Decodable, Encodable as _},
     io::Cursor,
     key::Secp256k1,
     relative::Height,
-    secp256k1::SecretKey,
-    Address, Amount, CompressedPublicKey, Network, OutPoint, Transaction, TxOut,
+    secp256k1::{All, PublicKey, SecretKey},
+    Address, Amount, CompressedPublicKey, Network, OutPoint, Transaction, TxOut, Txid,
 };
 use bitcoin_splitter::split::script::{IOPair, SplitableScript};
 use bitcoin_testscripts::{
@@ -16,16 +16,23 @@ use bitcoincore_rpc::{
     bitcoin::consensus::{Decodable as _, Encodable as _},
     RawTx as _, RpcApi,
 };
-use bitvm2_core::assert::{AssertTransaction, Options};
+use bitvm2_core::{
+    assert::{AssertTransaction, Options},
+    treepp::*,
+};
 use once_cell::sync::Lazy;
-use tracing_subscriber::fmt::format;
 
 use crate::common::{init_bitcoin_client, init_wallet};
 
-static SECKEY: Lazy<SecretKey> = Lazy::new(|| {
+static OPERATOR_SECKEY: Lazy<SecretKey> = Lazy::new(|| {
     "50c8f972285ad27527d79c80fe4df1b63c1192047713438b45758ea4e110a88b"
         .parse()
         .unwrap()
+});
+
+static OPERATOR_PUBKEY: Lazy<PublicKey> = Lazy::new(|| {
+    let ctx = Secp256k1::new();
+    OPERATOR_SECKEY.public_key(&ctx)
 });
 
 macro_rules! hex {
@@ -45,24 +52,29 @@ macro_rules! txconv {
     }};
 }
 
-fn test_script_payout_spending<const I: usize, const O: usize, S>() -> eyre::Result<()>
+struct TestSetup {
+    ctx: Secp256k1<All>,
+    client: bitcoincore_rpc::Client,
+    funder_address: bitcoincore_rpc::bitcoin::Address,
+    input_script: Script,
+    _output_script: Script,
+    funding_txid: Txid,
+    funding_txout_idx: usize,
+    funding_txout: TxOut,
+}
+
+fn setup_test<const I: usize, const O: usize, S>(amount_sats: u64) -> eyre::Result<TestSetup>
 where
     S: SplitableScript<I, O>,
 {
-    color_eyre::install()?;
-    tracing_subscriber::fmt().init();
-
     let client = init_bitcoin_client()?;
-
     let address = init_wallet()?;
 
-    let IOPair { input, .. } = S::generate_invalid_io_pair();
+    let IOPair { input, output } = S::generate_invalid_io_pair();
 
     let ctx = Secp256k1::new();
 
-    let operator_pubkey = SECKEY.public_key(&ctx);
-    let operator_xonly = operator_pubkey.x_only_public_key().0;
-
+    let operator_pubkey = OPERATOR_SECKEY.public_key(&ctx);
     let operator_p2wpkh_addr = Address::p2wpkh(
         &CompressedPublicKey::try_from(bitcoin::PublicKey::new(operator_pubkey)).unwrap(),
         Network::Regtest,
@@ -73,7 +85,7 @@ where
         &bitcoincore_rpc::bitcoin::Address::from_str(&operator_p2wpkh_addr.to_string())
             .unwrap()
             .assume_checked(),
-        bitcoincore_rpc::bitcoin::Amount::from_sat(71_000),
+        bitcoincore_rpc::bitcoin::Amount::from_sat(amount_sats),
         None,
         None,
         None,
@@ -89,8 +101,7 @@ where
         Transaction::consensus_decode(&mut cursor)?
     };
 
-    println!("Txid: {}", operator_funding_txid);
-    println!("Funding: {}", hex!(tx));
+    tracing::info!(hex = %hex!(tx), txid = %operator_funding_txid, "Created funding");
     client.generate_to_address(6, &address)?;
 
     // find txout
@@ -99,11 +110,39 @@ where
         .output
         .into_iter()
         .enumerate()
-        .find(|(_idx, out)| out.value == Amount::from_sat(71_000))
+        .find(|(_idx, out)| out.value == Amount::from_sat(amount_sats))
         .unwrap();
 
+    Ok(TestSetup {
+        ctx,
+        client,
+        funder_address: address,
+        input_script: input,
+        _output_script: output,
+        funding_txid: txid,
+        funding_txout_idx: idx,
+        funding_txout,
+    })
+}
+
+fn test_script_payout_spending<const I: usize, const O: usize, S>() -> eyre::Result<()>
+where
+    S: SplitableScript<I, O>,
+{
+    let TestSetup {
+        ctx,
+        client,
+        input_script,
+        funding_txid,
+        funding_txout_idx,
+        funding_txout,
+        funder_address,
+        ..
+    } = setup_test::<I, O, S>(71_000)?;
+
+    let operator_xonly = OPERATOR_PUBKEY.x_only_public_key().0;
     let assert_tx = AssertTransaction::<{ I }, { O }, S>::with_options(
-        input,
+        input_script,
         operator_xonly,
         Amount::from_sat(70_000),
         Options {
@@ -113,16 +152,15 @@ where
 
     let atx = assert_tx.clone().spend_p2wpkh_input_tx(
         &ctx,
-        &SECKEY,
+        &OPERATOR_SECKEY,
         funding_txout.clone(),
-        OutPoint::new(txid, idx as u32),
+        OutPoint::new(funding_txid, funding_txout_idx as u32),
     )?;
 
     println!("Txid: {}", atx.compute_txid());
-    // println!("AssertSize: {}", hex!(atx).len());
     println!("Assert: {}", hex!(atx));
     client.send_raw_transaction(txconv!(atx))?;
-    client.generate_to_address(1, &address)?;
+    client.generate_to_address(1, &funder_address)?;
 
     let payout_tx = assert_tx.payout_transaction(
         &ctx,
@@ -131,13 +169,13 @@ where
             script_pubkey: funding_txout.script_pubkey,
         },
         atx.compute_txid(),
-        &SECKEY,
+        &OPERATOR_SECKEY,
     )?;
 
     println!("Txid: {}", payout_tx.compute_txid());
     println!("Payout: {}", hex!(payout_tx));
     client.send_raw_transaction(txconv!(payout_tx))?;
-    client.generate_to_address(6, &address)?;
+    client.generate_to_address(6, &funder_address)?;
 
     Ok(())
 }
@@ -146,64 +184,22 @@ fn test_script_disprove_distorted<const I: usize, const O: usize, S>() -> eyre::
 where
     S: SplitableScript<I, O>,
 {
-    color_eyre::install()?;
-    let format = format().pretty();
-    tracing_subscriber::fmt().event_format(format).init();
+    let TestSetup {
+        ctx,
+        client,
+        input_script,
+        funding_txid,
+        funding_txout_idx,
+        funding_txout,
+        funder_address,
+        ..
+    } = setup_test::<I, O, S>(100_000)?;
 
-    let client = init_bitcoin_client()?;
-
-    let address = init_wallet()?;
-
-    let IOPair { input, .. } = S::generate_invalid_io_pair();
-
-    let ctx = Secp256k1::new();
-
-    let operator_pubkey = SECKEY.public_key(&ctx);
-    let operator_xonly = operator_pubkey.x_only_public_key().0;
-
-    let operator_p2wpkh_addr = Address::p2wpkh(
-        &CompressedPublicKey::try_from(bitcoin::PublicKey::new(operator_pubkey)).unwrap(),
-        Network::Regtest,
-    );
-
-    // TODO(Velnbur): fix version of bitcoincorerpc and Bitcoin for this...
-    let operator_funding_txid = client.send_to_address(
-        &bitcoincore_rpc::bitcoin::Address::from_str(&operator_p2wpkh_addr.to_string())
-            .unwrap()
-            .assume_checked(),
-        bitcoincore_rpc::bitcoin::Amount::from_sat(100_000),
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-    )?;
-    let tx = client.get_raw_transaction(&operator_funding_txid, None)?;
-    let tx = {
-        let mut buf = Vec::new();
-        tx.consensus_encode(&mut buf).unwrap();
-        let mut cursor = Cursor::new(&buf);
-        Transaction::consensus_decode(&mut cursor)?
-    };
-
-    println!("Txid: {}", operator_funding_txid);
-    println!("Funding: {}", hex!(tx));
-    client.generate_to_address(6, &address)?;
-
-    // find txout
-    let txid = tx.compute_txid();
-    let (idx, funding_txout) = tx
-        .output
-        .into_iter()
-        .enumerate()
-        .find(|(_idx, out)| out.value == Amount::from_sat(100_000))
-        .unwrap();
-
-    let (assert_tx, _distored_idx) = AssertTransaction::<{ I }, { O }, S>::with_options_distorted(
-        input,
+    let operator_xonly = OPERATOR_PUBKEY.x_only_public_key().0;
+    let (assert_tx, distored_idx) = AssertTransaction::<{ I }, { O }, S>::with_options_distorted(
+        input_script,
         operator_xonly,
-        Amount::from_sat(99_000),
+        Amount::from_sat(90_000),
         Options {
             payout_locktime: Height::from(1),
         },
@@ -211,16 +207,15 @@ where
 
     let atx = assert_tx.clone().spend_p2wpkh_input_tx(
         &ctx,
-        &SECKEY,
+        &OPERATOR_SECKEY,
         funding_txout.clone(),
-        OutPoint::new(txid, idx as u32),
+        OutPoint::new(funding_txid, funding_txout_idx as u32),
     )?;
 
     println!("Txid: {}", atx.compute_txid());
-    // println!("AssertSize: {}", hex!(atx).len());
     println!("Assert: {}", hex!(atx));
     client.send_raw_transaction(txconv!(atx))?;
-    client.generate_to_address(1, &address)?;
+    client.generate_to_address(1, &funder_address)?;
 
     let disprove_txs = assert_tx.clone().disprove_transactions(
         &ctx,
@@ -231,27 +226,20 @@ where
         atx.compute_txid(),
     )?;
 
-    let number = disprove_txs.len();
-    tracing::info!("Number of disprove txs: {}", number);
-    for (idx, (_disprove_script, disprove_tx)) in disprove_txs.into_iter().enumerate() {
-        let err = client.send_raw_transaction(txconv!(disprove_tx)).err();
+    let disprove_tx = disprove_txs
+        .get(&assert_tx.disprove_scripts[distored_idx])
+        .unwrap();
+    client.send_raw_transaction(txconv!(disprove_tx))?;
 
-        if let Some(err) = err {
-            tracing::warn!(reason = %err, "Disprove Tx {idx} rejected...");
-            if idx + 1 == number {
-                panic!("no dispove transaction worked!");
-            }
-            continue;
-        }
+    let hexed = hex!(disprove_tx);
+    println!("Txid: {}", disprove_tx.compute_txid());
+    println!("DisproveSize: {}", hexed.len() / 2);
 
-        let hexed = hex!(disprove_tx);
-        println!("Txid: {}", disprove_tx.compute_txid());
-        println!("DisproveSize: {}", hexed.len() / 2);
-        fs::write("./disprove.hex", hexed)?;
-        break;
+    if env::var("NERO_TESTS_TX_FILE").is_ok() {
+        fs::write("./disprove.txt", hexed)?;
     }
 
-    client.generate_to_address(6, &address)?;
+    client.generate_to_address(6, &funder_address)?;
 
     Ok(())
 }
@@ -259,6 +247,8 @@ where
 #[test]
 #[ignore = "Stack size limit exceeded"]
 fn test_u254_mul_disprove() -> eyre::Result<()> {
+    color_eyre::install()?;
+    tracing_subscriber::fmt().init();
     test_script_disprove_distorted::<
         { U254MulScript::INPUT_SIZE },
         { U254MulScript::OUTPUT_SIZE },
@@ -268,6 +258,8 @@ fn test_u254_mul_disprove() -> eyre::Result<()> {
 
 #[test]
 fn test_u254_mul_payout() -> eyre::Result<()> {
+    color_eyre::install()?;
+    tracing_subscriber::fmt().init();
     test_script_payout_spending::<
         { U254MulScript::INPUT_SIZE },
         { U254MulScript::OUTPUT_SIZE },
@@ -276,7 +268,9 @@ fn test_u254_mul_payout() -> eyre::Result<()> {
 }
 
 #[test]
-fn test_square_fibonachi_disprove() -> eyre::Result<()> {
+fn test_square_fibonachi() -> eyre::Result<()> {
+    color_eyre::install()?;
+    tracing_subscriber::fmt().init();
     test_script_disprove_distorted::<
         { SquareFibonacciScript::<1024>::INPUT_SIZE },
         { SquareFibonacciScript::<1024>::OUTPUT_SIZE },
@@ -285,7 +279,10 @@ fn test_square_fibonachi_disprove() -> eyre::Result<()> {
 }
 
 #[test]
+#[ignore = "Stack size limit exceeded"]
 fn test_u32mul_disprove() -> eyre::Result<()> {
+    color_eyre::install()?;
+    tracing_subscriber::fmt().init();
     test_script_disprove_distorted::<
         { U32MulScript::INPUT_SIZE },
         { U32MulScript::OUTPUT_SIZE },
